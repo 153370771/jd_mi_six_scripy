b426fab3a186c09c4712803195a423fabb0a23c1
Tony~ <wlw9393@gmail.com>
2017-05-25 10:55:25 +0800

    update jd.py
    加载用户数据，避免出现data；
    禁止加载图片，提高加载速度。

--- jd.py
+++ jd.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 #coding:utf-8
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import datetime
 #下面填入京东的用户名以及密码
 jd_up={"ue":"****","pd":"****"}
 
-chrome=webdriver.Chrome()
+opts = Options()
+opts.add_argument("--user-data-dir=C:\Users\mushhi\AppData\Local\Google\Chrome\User Data\Default")#load user data
+prefs = {"profile.managed_default_content_settings.images":2} # this will disable image loading in the browser
+opts.add_experimental_option("prefs",prefs)  # Added preference into chrome options
+opts.add_argument("user-agent="'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36')
+#added chrome option user-agent
+chrome = webdriver.Chrome('chromedriver.exe',chrome_options=opts) # finally add these option as startup option for Google Chrome
 chrome.get(url="https://passport.jd.com/new/login.aspx?")
 chrome.find_element_by_xpath("//*[@id=\"content\"]/div/div[1]/div/div[2]/a").click()
 User=chrome.find_element_by_id("loginname")
@@ -23,3 +31,5 @@ while True:
         chrome.quit()
     except Exception:
         print("还未开始抢购！！")
+        time_stamp = datetime.datetime.now()
+        print "现在时间       " + time_stamp.strftime('%Y.%m.%d-%H:%M:%S')
